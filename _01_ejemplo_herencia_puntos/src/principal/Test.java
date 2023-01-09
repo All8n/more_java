87c7200ed0d895ce5909e81987db528c23077692
manana <manana@A13E22>
2023-01-09 13:51:11 +0100

    09

---
+++ _01_ejemplo_herencia_puntos/src/principal/Test.java
@@ -0,0 +1,25 @@
+package principal;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//creamos dos objetos Puntos y dibujamos
+		
+		/*Punto p1=new Punto();
+		Punto p2=new Punto(3,7);
+		p1.dibujar();
+		p2.dibujar();*/
+		
+		//creamos un objeto Punto3D y llamamos a sus metodos
+		
+		Punto3D pd=new Punto3D(1,7,4);
+		
+		pd.dibujar();
+		pd.mover(2);
+		System.out.println( "------");
+		pd.dibujar();
+		
+		
+	}
+
+}
