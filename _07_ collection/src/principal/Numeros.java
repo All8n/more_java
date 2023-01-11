2e14b2efde537c35250f53ec5d953c5f546833c8
manana <manana@A13E22>
2023-01-11 13:58:51 +0100

    11

--- _07_ collection/src/principal/Numeros.java
+++ _07_ collection/src/principal/Numeros.java
@@ -16,4 +16,15 @@ public class Numeros {
 	//realizar un segundo metodo , que reciba dos colecciones de numeros
 	//y nos diga cuantos numeros se encuentran en ambas selecciones
 
+	public int recibirCollection(Collection<Integer>col1,Collection<Integer>col2) {
+		int rep=0;
+		for(Integer n:col1) {//recorre la primera coleccion
+			if(col2.contains(n)) {//si la coleccion 2 contiene algo de la coleccion 1
+				rep++;//aumenta
+			}
+		}
+		return rep;
+
+	}
 }
+
